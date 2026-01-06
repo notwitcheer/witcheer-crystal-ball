d0e224a210ad0b91000e893464ce57a555be3714
witcheer <witcheer.eth@gmail.com>
2026-01-06 09:29:11 +0100

    modified main structure to fit login standards

--- src/main.py
+++ src/main.py
@@ -36,18 +36,13 @@ from .alerts.telegram import TelegramAlerter
 # Initialize structured logging
 structlog.configure(
     processors=[
-        structlog.stdlib.filter_by_level,
-        structlog.stdlib.add_logger_name,
-        structlog.stdlib.add_log_level,
-        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.add_log_level,
         structlog.processors.TimeStamper(fmt="iso"),
         structlog.processors.StackInfoRenderer(),
         structlog.processors.format_exc_info,
-        structlog.processors.UnicodeDecoder(),
-        structlog.dev.ConsoleRenderer() if not get_settings().log_json_format 
+        structlog.dev.ConsoleRenderer() if not get_settings().log_json_format
             else structlog.processors.JSONRenderer()
     ],
-    wrapper_class=structlog.stdlib.BoundLogger,
     context_class=dict,
     logger_factory=structlog.PrintLoggerFactory(),
     cache_logger_on_first_use=True,
